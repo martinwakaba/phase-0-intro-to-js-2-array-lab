14dd5af090e23b0f4aa26bab9aa9140ea3c54f37
MartinWakaba <martin.wakaba@student.moringaschool.com>
2023-09-28 13:04:18 +0300

    Assignment Done

--- index.js
+++ index.js
@@ -1,1 +1,49 @@
 // Write your solution here!
+
+const cats = ["Milo", "Otis", "Garfield"];
+
+function destructivelyAppendCat(name){
+
+    cats.push("Ralph");
+}
+function destructivelyPrependCat(name){
+    cats.unshift("Bob");
+}
+
+function removeLastCat(){
+    const copyOfcats = [...cats];
+
+    copyOfcats.pop();
+
+    return copyOfcats;
+}
+
+function removeFirstCat(){
+    const copyOfcats = [...cats];
+
+    copyOfcats.shift();
+
+    return copyOfcats;
+}
+
+function destructivelyRemoveFirstCat(){
+    cats.splice(0,1);
+    
+}
+
+function destructivelyRemoveLastCat(){
+    cats.splice(cats.length - 1);
+    
+}
+
+function appendCat(name){
+    const newCats = [...cats];
+    newCats.splice(newCats.length, 0, "Broom");
+    return newCats;
+}
+
+function prependCat(name){
+    const newCats = [...cats];
+    newCats.splice(0, 0, "Arnold");
+    return newCats;
+}
